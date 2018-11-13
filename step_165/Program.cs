0071799295396034b1ea67811c593002c743ffd6
liammcdonald97 <liammcdonald97@gmail.com>
2018-11-13 16:24:25 -0500

    Add project files.

---
+++ step_165/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace step_165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            try
+            {
+                Console.WriteLine("Please enter your age:");
+                int userInput1 = Convert.ToInt32(Console.ReadLine());
+                int currentYear = Convert.ToInt32(DateTime.Now.Year.ToString());
+                int yearBorn = currentYear - userInput1;
+                int yearBornAlternate = yearBorn - 1;
+                Console.WriteLine("You were born in {0}/{1}", yearBornAlternate, yearBorn);
+                if (userInput1 == 0)
+                {
+                    throw new ArgumentException();           
+                }
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Your age cannot be zero.");               
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a whole number:");                            
+            }    
+            
+            Console.ReadLine();
+
+        }
+    }
+}
